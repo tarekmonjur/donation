723304edae514c102f50d35a5628e48cdc9d2ca9
tarekmonjur <tarekmonjur@gmail.com>
2018-01-22 16:22:59 +0600

    header update

--- resources/views/layouts/common/header.blade.php
+++ resources/views/layouts/common/header.blade.php
@@ -5,7 +5,12 @@
             <span class="navbar-toggler-icon"></span>
         </button>
 
+        <a class="navbar-brand" href="#">@if(isset(session('company_account')->companyName)) {{session('company_account')->companyName}} @endif</a>
+
         <ul class="nav navbar-nav justify-content-end">
+            <li class="nav-item">
+                <a class="nav-link active" href="#">{{ucfirst($auth->full_name)}} ( {{ucfirst($auth->user_type)}} )</a>
+            </li>
             <li class="nav-item">
                 <a class="nav-link active" href="{{url('logout')}}">Logout</a>
             </li>
